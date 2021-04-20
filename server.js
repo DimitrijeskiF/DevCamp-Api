8b1fc446802134637454ac1e988d8bbd84b705cc
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2021-04-21 00:17:47 +0200

    Basic Crud

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-//Load env vars
+
+
+
 dotenv.config({ path: './config/config.env' });
+connectDB();
 
+const bootcamps = require('./routes/bootcamps')
 const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 3000
 
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT} and is running in ${process.env.NODE_ENV} mode!`);
+const server = app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT} and is running in ${process.env.NODE_ENV} mode!`.yellow.bold);
 });
+
+
+//Handling unhandled promise rejections
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+
+    //Close server
+    server.close(() => {
+        process.exit(1)
+    })
+})
