ffaec520b410a214f8f6f421cb16101c27a6bbbe
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2021-04-30 12:48:31 +0200

    AddCourse

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -25,7 +25,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
     //Finding Resources
-    query = Bootcamp.find(JSON.parse(queryStr));
+    query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
     // Select Fields
     if (req.query.select) {
@@ -114,6 +114,7 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
 
+
     res.status(200).json({
         success: true,
         data: bootcamp
@@ -121,11 +122,12 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 })
 
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404))
     }
+    bootcamp.remove();
 
     res.status(200).json({
         success: true,
