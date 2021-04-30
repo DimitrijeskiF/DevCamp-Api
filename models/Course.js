256e3bfbab15148d90961d6a07076f503aefa730
Filip Dimitrijeski <filipdimitrijeski7@gmail.com>
2021-04-30 19:52:05 +0200

    AverageCost Aggreagtions

--- models/Course.js
+++ models/Course.js
@@ -35,8 +35,49 @@ const CourseSchema = new mongoose.Schema({
         ref: 'Bootcamp',
         required: true
     }
+});
+
+
+//Static method to get avg of course tuitions
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+    console.log('Calculating avg cost...'.blue);
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId }
+        },
+        {
+            $group: {
+                _id: '$bootcamp',
+                averageCost: { $avg: '$tuition' }
+            }
+        }
+    ]);
+
+
+    try {
+        await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+            averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+//getAverage cost after save
+
+CourseSchema.post('save', function () {
+    this.constructor.getAverageCost(this.bootcamp);
+})
+
+//Before removing course
+
+CourseSchema.pre('remove', function () {
+    this.constructor.getAverageCost(this.bootcamp);
 })
 
 
 
-module.exports = mongoose.model('Course', CourseSchema); 
+
+module.exports = mongoose.model('Course', CourseSchema);
